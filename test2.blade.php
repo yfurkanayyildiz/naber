5539a89407433723700c54f11c4e676b3fc468f4
yfurkanayyildiz <yusuffurkanayyildiz@gmail.com>
2022-04-25 18:33:32 +0300

    first commit

---
+++ test2.blade.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is Test parameters</h1>
+Id Value : {{$id}}
+<br>
+Number Value: {{$number}}
+<br>
+Sum Params :{{$id + $number}}
+</body>
+</html>
---
+++ test3.blade.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+<body>
+
+<h1>This is Test parameters</h1>
+First Name : {{$fname}}
+<br>
+Last Name : {{$lname}}
+</body>
+</html>
